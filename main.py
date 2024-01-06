2c1d82fdd130d0763ed64bfc29c45a0446f698d4
Ronald van Butselaar <rvanbutselaar@ilionx.com>
2024-01-06 22:22:12 +0100

    refactoring

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from prometheus_client import start_http_server
 from prometheus_client.core import REGISTRY, GaugeMetricFamily
 from smeterd.meter import SmartMeter
 
-'''
+"""
 Inspired by:
 * https://github.com/sanderjo/P1/blob/master/P1-parser.awk
 * https://github.com/prometheus/client_python
@@ -23,28 +23,38 @@ Inspired by:
 '1-0:52.7.0':'p1_l2_voltage',
 '1-0:72.7.0':'p1_l3_voltage',
 '0-0:96.14.0':'p1_current_tarrif' ( 2 is high)
-'''
+"""
 
 
 def markup_helper(str_line):
-    '''
+    """
     Read raw string and return only the value
-    '''
-    return int(str_line.split('(')[-1].split('*')[0].replace('.', ''))
+    """
+    return int(str_line.split("(")[-1].split("*")[0].replace(".", ""))
 
 
 def markup_helper_float(str_line):
-    '''
+    """
     Read raw string and return only the value
-    '''
-    return float(str_line.split('(')[-1].split('*')[0])
+    """
+    return float(str_line.split("(")[-1].split("*")[0])
 
 
 def markup_helper_tarrif(str_line):
-    '''
+    """
     Read raw string and return only the value
-    '''
-    return int(str_line.split('(')[-1].replace(')', '').replace('0', ''))
+    """
+    return int(str_line.split("(")[-1].replace(")", "").replace("0", ""))
+
+
+def metric_helper(metric_name, p1_line, metrics):
+    """
+    Create a metric object
+    """
+    logging.debug(f"{{ metric_name }}: {{ p1_line }}")
+    metrics[metric_name].add_metric(["Rozensingel"], markup_helper(p1_line))
+
+    return metrics
 
 
 class CustomCollector(object):
@@ -54,74 +64,117 @@ class CustomCollector(object):
 
         for p1_line in p1_list:
             # logging.info(p1_line)
-            if '1-0:1.8.1' in p1_line:
-                logging.debug("p1_total_electricity_used_rate_1: {}".format(
-                    markup_helper(p1_line)))
-                metrics['p1_total_electricity_used_rate_1'].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line))
-            elif '1-0:1.8.2' in p1_line:
-                logging.debug("p1_total_electricity_used_rate_2: {}".format(
-                    markup_helper(p1_line)))
-                metrics['p1_total_electricity_used_rate_2'].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line))
-            elif '1-0:2.8.1' in p1_line:
-                logging.debug("p1_total_electricity_provided_rate_1: {}".format(
-                    markup_helper(p1_line)))
-                metrics['p1_total_electricity_provided_rate_1'].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line))
-            elif '1-0:2.8.2' in p1_line:
-                logging.debug("p1_total_electricity_provided_rate_2: {}".format(
-                    markup_helper(p1_line)))
-                metrics['p1_total_electricity_provided_rate_2'].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line))
-            elif '1-0:1.7.0' in p1_line:
-                logging.debug("p1_total_electricity_used: {}".format(
-                    markup_helper(p1_line)))
-                metrics['p1_total_electricity_used'].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line))
-            elif '1-0:2.7.0' in p1_line:
-                logging.debug("p1_total_electricity_provided: {}".format(
-                    markup_helper(p1_line)))
-                metrics['p1_total_electricity_provided'].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line))
-            elif '1-0:32.7.0' in p1_line:
-                logging.debug("p1_l1_voltage: {}".format(
-                    markup_helper_float(p1_line)))
-                metrics['p1_l1_voltage'].add_metric(
-                    ["Rozensingel"], markup_helper_float(p1_line))
-            elif '1-0:52.7.0' in p1_line:
-                logging.debug("p1_l2_voltage: {}".format(
-                    markup_helper_float(p1_line)))
-                metrics['p1_l2_voltage'].add_metric(
-                    ["Rozensingel"], markup_helper_float(p1_line))
-            elif '1-0:72.7.0' in p1_line:
-                logging.debug("p1_l3_voltage: {}".format(
-                    markup_helper_float(p1_line)))
-                metrics['p1_l3_voltage'].add_metric(
-                    ["Rozensingel"], markup_helper_float(p1_line))
-            elif '0-0:96.14.0' in p1_line:
-                logging.debug("p1_current_tarrif: {}".format(
-                    markup_helper_tarrif(p1_line)))
-                metrics['p1_current_tarrif'].add_metric(
-                    ["Rozensingel"], markup_helper_tarrif(p1_line))
+            if "1-0:1.8.1" in p1_line:
+                metric_helper("p1_total_electricity_used_rate_1", p1_line, metrics)
+                # logging.debug(
+                #     "p1_total_electricity_used_rate_1: {}".format(
+                #         markup_helper(p1_line)
+                #     )
+                # )
+                # metrics["p1_total_electricity_used_rate_1"].add_metric(
+                #     ["Rozensingel"], markup_helper(p1_line)
+                # )
+            elif "1-0:1.8.2" in p1_line:
+                logging.debug(
+                    "p1_total_electricity_used_rate_2: {}".format(
+                        markup_helper(p1_line)
+                    )
+                )
+                metrics["p1_total_electricity_used_rate_2"].add_metric(
+                    ["Rozensingel"], markup_helper(p1_line)
+                )
+            elif "1-0:2.8.1" in p1_line:
+                logging.debug(
+                    "p1_total_electricity_provided_rate_1: {}".format(
+                        markup_helper(p1_line)
+                    )
+                )
+                metrics["p1_total_electricity_provided_rate_1"].add_metric(
+                    ["Rozensingel"], markup_helper(p1_line)
+                )
+            elif "1-0:2.8.2" in p1_line:
+                logging.debug(
+                    "p1_total_electricity_provided_rate_2: {}".format(
+                        markup_helper(p1_line)
+                    )
+                )
+                metrics["p1_total_electricity_provided_rate_2"].add_metric(
+                    ["Rozensingel"], markup_helper(p1_line)
+                )
+            elif "1-0:1.7.0" in p1_line:
+                logging.debug(
+                    "p1_total_electricity_used: {}".format(markup_helper(p1_line))
+                )
+                metrics["p1_total_electricity_used"].add_metric(
+                    ["Rozensingel"], markup_helper(p1_line)
+                )
+            elif "1-0:2.7.0" in p1_line:
+                logging.debug(
+                    "p1_total_electricity_provided: {}".format(markup_helper(p1_line))
+                )
+                metrics["p1_total_electricity_provided"].add_metric(
+                    ["Rozensingel"], markup_helper(p1_line)
+                )
+            elif "1-0:32.7.0" in p1_line:
+                logging.debug("p1_l1_voltage: {}".format(markup_helper_float(p1_line)))
+                metrics["p1_l1_voltage"].add_metric(
+                    ["Rozensingel"], markup_helper_float(p1_line)
+                )
+            elif "1-0:52.7.0" in p1_line:
+                logging.debug("p1_l2_voltage: {}".format(markup_helper_float(p1_line)))
+                metrics["p1_l2_voltage"].add_metric(
+                    ["Rozensingel"], markup_helper_float(p1_line)
+                )
+            elif "1-0:72.7.0" in p1_line:
+                logging.debug("p1_l3_voltage: {}".format(markup_helper_float(p1_line)))
+                metrics["p1_l3_voltage"].add_metric(
+                    ["Rozensingel"], markup_helper_float(p1_line)
+                )
+            elif "0-0:96.14.0" in p1_line:
+                logging.debug(
+                    "p1_current_tarrif: {}".format(markup_helper_tarrif(p1_line))
+                )
+                metrics["p1_current_tarrif"].add_metric(
+                    ["Rozensingel"], markup_helper_tarrif(p1_line)
+                )
 
         return metrics
 
     def collect(self):
         metrics = {
-            'p1_total_electricity_used_rate_1': GaugeMetricFamily('p1_total_electricity_used_rate_1', 'Gebruik elektra dal'),
-            'p1_total_electricity_used_rate_2': GaugeMetricFamily('p1_total_electricity_used_rate_2', 'Gebruik elektra piek'),
-            'p1_total_electricity_provided_rate_1': GaugeMetricFamily('p1_total_electricity_provided_rate_1', 'Geleverd elektra dal'),
-            'p1_total_electricity_provided_rate_2': GaugeMetricFamily('p1_total_electricity_provided_rate_2', 'Geleverd elektra piek'),
-            'p1_total_electricity_used': GaugeMetricFamily('p1_total_electricity_used', 'Totaal gebruik elektra'),
-            'p1_total_electricity_provided': GaugeMetricFamily('p1_total_electricity_provided', 'Totaal geleverd elektra'),
-            'p1_l1_voltage': GaugeMetricFamily('p1_l1_voltage', 'Totaal geleverd elektra'),
-            'p1_l2_voltage': GaugeMetricFamily('p1_l2_voltage', 'Totaal geleverd elektra'),
-            'p1_l3_voltage': GaugeMetricFamily('p1_l3_voltage', 'Totaal geleverd elektra'),
-            'p1_current_tarrif': GaugeMetricFamily('p1_current_tarrif', 'Totaal geleverd elektra')
+            "p1_total_electricity_used_rate_1": GaugeMetricFamily(
+                "p1_total_electricity_used_rate_1", "Gebruik elektra dal"
+            ),
+            "p1_total_electricity_used_rate_2": GaugeMetricFamily(
+                "p1_total_electricity_used_rate_2", "Gebruik elektra piek"
+            ),
+            "p1_total_electricity_provided_rate_1": GaugeMetricFamily(
+                "p1_total_electricity_provided_rate_1", "Geleverd elektra dal"
+            ),
+            "p1_total_electricity_provided_rate_2": GaugeMetricFamily(
+                "p1_total_electricity_provided_rate_2", "Geleverd elektra piek"
+            ),
+            "p1_total_electricity_used": GaugeMetricFamily(
+                "p1_total_electricity_used", "Totaal gebruik elektra"
+            ),
+            "p1_total_electricity_provided": GaugeMetricFamily(
+                "p1_total_electricity_provided", "Totaal geleverd elektra"
+            ),
+            "p1_l1_voltage": GaugeMetricFamily(
+                "p1_l1_voltage", "Totaal geleverd elektra"
+            ),
+            "p1_l2_voltage": GaugeMetricFamily(
+                "p1_l2_voltage", "Totaal geleverd elektra"
+            ),
+            "p1_l3_voltage": GaugeMetricFamily(
+                "p1_l3_voltage", "Totaal geleverd elektra"
+            ),
+            "p1_current_tarrif": GaugeMetricFamily(
+                "p1_current_tarrif", "Totaal geleverd elektra"
+            ),
         }
 
-        meter = SmartMeter('/dev/ttyUSB0', baudrate=115200)
+        meter = SmartMeter("/dev/ttyUSB0", baudrate=115200)
         metrics = self.get_p1_metrics(meter.read_one_packet(), metrics)
         meter.disconnect()
 
@@ -129,11 +182,12 @@ class CustomCollector(object):
             yield metric
 
 
-if __name__ == '__main__':
-    LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
+if __name__ == "__main__":
+    LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
     logging.info("Starting ESMR5 metrics exporter")
     logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s', level=LOGLEVEL)
+        format="%(asctime)s - %(levelname)s - %(message)s", level=LOGLEVEL
+    )
 
     REGISTRY.register(CustomCollector())
     start_http_server(8000)
