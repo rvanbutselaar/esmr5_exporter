5efacad5ead7ca1afb643403c01165398eed28cf
Ronald van Butselaar <rvanbutselaar@ilionx.com>
2024-01-06 22:56:58 +0100

    Some more refactoring

--- main.py
+++ main.py
@@ -63,114 +63,76 @@ class CustomCollector(object):
         p1_list = str(p1_lines).splitlines()
 
         for p1_line in p1_list:
-            # logging.info(p1_line)
+            # Meter Reading electricity delivered to client (low tariff) in 0,001 kWh
             if "1-0:1.8.1" in p1_line:
                 metric_helper("p1_total_electricity_used_rate_1", p1_line, metrics)
-                # logging.debug(
-                #     "p1_total_electricity_used_rate_1: {}".format(
-                #         markup_helper(p1_line)
-                #     )
-                # )
-                # metrics["p1_total_electricity_used_rate_1"].add_metric(
-                #     ["Rozensingel"], markup_helper(p1_line)
-                # )
+            # Meter Reading electricity delivered to client (normal tariff) in 0,001 kWh
             elif "1-0:1.8.2" in p1_line:
-                logging.debug(
-                    "p1_total_electricity_used_rate_2: {}".format(
-                        markup_helper(p1_line)
-                    )
-                )
-                metrics["p1_total_electricity_used_rate_2"].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line)
-                )
+                metric_helper("p1_total_electricity_used_rate_2", p1_line, metrics)
+            # Meter Reading electricity delivered by client (low tariff) in 0,001 kWh
             elif "1-0:2.8.1" in p1_line:
-                logging.debug(
-                    "p1_total_electricity_provided_rate_1: {}".format(
-                        markup_helper(p1_line)
-                    )
-                )
-                metrics["p1_total_electricity_provided_rate_1"].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line)
-                )
+                metric_helper("p1_total_electricity_provided_rate_1", p1_line, metrics)
+            # Meter Reading electricity delivered by client (normal tariff) in 0,001 kWh
             elif "1-0:2.8.2" in p1_line:
-                logging.debug(
-                    "p1_total_electricity_provided_rate_2: {}".format(
-                        markup_helper(p1_line)
-                    )
-                )
-                metrics["p1_total_electricity_provided_rate_2"].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line)
-                )
+                metric_helper("p1_total_electricity_provided_rate_2", p1_line, metrics)
+            # Actual electricity power delivered (+P) in 1 Watt resolution
             elif "1-0:1.7.0" in p1_line:
-                logging.debug(
-                    "p1_total_electricity_used: {}".format(markup_helper(p1_line))
-                )
-                metrics["p1_total_electricity_used"].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line)
-                )
+                metric_helper("p1_total_electricity_used", p1_line, metrics)
+            # Actual electricity power received (-P) in 1 Watt resolution
             elif "1-0:2.7.0" in p1_line:
-                logging.debug(
-                    "p1_total_electricity_provided: {}".format(markup_helper(p1_line))
-                )
-                metrics["p1_total_electricity_provided"].add_metric(
-                    ["Rozensingel"], markup_helper(p1_line)
-                )
+                metric_helper("p1_total_electricity_provided", p1_line, metrics)
+            # Instantaneous voltage L1
             elif "1-0:32.7.0" in p1_line:
-                logging.debug("p1_l1_voltage: {}".format(markup_helper_float(p1_line)))
-                metrics["p1_l1_voltage"].add_metric(
-                    ["Rozensingel"], markup_helper_float(p1_line)
-                )
+                metric_helper("p1_l1_voltage", p1_line, metrics)
+            # Instantaneous voltage L2
             elif "1-0:52.7.0" in p1_line:
-                logging.debug("p1_l2_voltage: {}".format(markup_helper_float(p1_line)))
-                metrics["p1_l2_voltage"].add_metric(
-                    ["Rozensingel"], markup_helper_float(p1_line)
-                )
+                metric_helper("p1_l2_voltage", p1_line, metrics)
+            # Instantaneous voltage L3
             elif "1-0:72.7.0" in p1_line:
-                logging.debug("p1_l3_voltage: {}".format(markup_helper_float(p1_line)))
-                metrics["p1_l3_voltage"].add_metric(
-                    ["Rozensingel"], markup_helper_float(p1_line)
-                )
+                metric_helper("p1_l3_voltage", p1_line, metrics)
+            # Tariff indicator electricity
             elif "0-0:96.14.0" in p1_line:
-                logging.debug(
-                    "p1_current_tarrif: {}".format(markup_helper_tarrif(p1_line))
-                )
-                metrics["p1_current_tarrif"].add_metric(
-                    ["Rozensingel"], markup_helper_tarrif(p1_line)
-                )
+                metric_helper("p1_current_tarrif", p1_line, metrics)
 
         return metrics
 
     def collect(self):
         metrics = {
             "p1_total_electricity_used_rate_1": GaugeMetricFamily(
-                "p1_total_electricity_used_rate_1", "Gebruik elektra dal"
+                "p1_total_electricity_used_rate_1",
+                "Meter Reading electricity delivered to client (low tariff) in 0,001 kWh",
             ),
             "p1_total_electricity_used_rate_2": GaugeMetricFamily(
-                "p1_total_electricity_used_rate_2", "Gebruik elektra piek"
+                "p1_total_electricity_used_rate_2",
+                "Meter Reading electricity delivered to client (normal tariff) in 0,001 kWh",
             ),
             "p1_total_electricity_provided_rate_1": GaugeMetricFamily(
-                "p1_total_electricity_provided_rate_1", "Geleverd elektra dal"
+                "p1_total_electricity_provided_rate_1",
+                "Meter Reading electricity delivered by client (low tariff) in 0,001 kWh",
             ),
             "p1_total_electricity_provided_rate_2": GaugeMetricFamily(
-                "p1_total_electricity_provided_rate_2", "Geleverd elektra piek"
+                "p1_total_electricity_provided_rate_2",
+                "Meter Reading electricity delivered by client (normal tariff) in 0,001 kWh",
             ),
             "p1_total_electricity_used": GaugeMetricFamily(
-                "p1_total_electricity_used", "Totaal gebruik elektra"
+                "p1_total_electricity_used",
+                "Actual electricity power delivered (+P) in 1 Watt resolution",
             ),
             "p1_total_electricity_provided": GaugeMetricFamily(
-                "p1_total_electricity_provided", "Totaal geleverd elektra"
+                "p1_total_electricity_provided",
+                "Actual electricity power received (-P) in 1 Watt resolution",
             ),
             "p1_l1_voltage": GaugeMetricFamily(
-                "p1_l1_voltage", "Totaal geleverd elektra"
+                "p1_l1_voltage", "Instantaneous voltage L1"
             ),
             "p1_l2_voltage": GaugeMetricFamily(
-                "p1_l2_voltage", "Totaal geleverd elektra"
+                "p1_l2_voltage", "Instantaneous voltage L3"
             ),
             "p1_l3_voltage": GaugeMetricFamily(
-                "p1_l3_voltage", "Totaal geleverd elektra"
+                "p1_l3_voltage", "Instantaneous voltage L3"
             ),
             "p1_current_tarrif": GaugeMetricFamily(
-                "p1_current_tarrif", "Totaal geleverd elektra"
+                "p1_current_tarrif", "Tariff indicator electricity"
             ),
         }
 
@@ -184,10 +146,10 @@ class CustomCollector(object):
 
 if __name__ == "__main__":
     LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
-    logging.info("Starting ESMR5 metrics exporter")
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(message)s", level=LOGLEVEL
     )
+    logging.info("Starting ESMR5 metrics exporter")
 
     REGISTRY.register(CustomCollector())
     start_http_server(8000)
